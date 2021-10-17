ca6b20ebcfa963505ae7d7466b16a4bf18a6ced8
jclee0109 <jclee0109@gist.ac.kr>
2021-10-17 19:40:52 +0900

    move to my local folder - tracking and jsonfield

---
+++ shortener/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from shortener.models import PayPlan, Users, Statistic
+
+# Register your models here.
+
+admin.site.register(PayPlan)
+admin.site.register(Users)
+admin.site.register(Statistic)
