4acda1367d0947d8d31e9db9b99ecb1ab609f9f7
jclee0109 <jclee0109@gist.ac.kr>
2021-10-19 16:04:00 +0900

    drf listview, CRUD

--- shortener/urls/apis.py
+++ shortener/urls/apis.py
@@ -1,13 +1,56 @@
-from rest_framework import viewsets, permissions
+from django.http import Http404
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 from shortener.models import ShortenedUrls
-from shortener.urls.serializers import UrlListSerializer
+from shortener.urls.serializers import UrlListSerializer, UrlCreateSerializer
+from shortener.utils import url_count_changer, MsgOk
 
 
 class UrlViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewd or edited
     """
-    queryset = ShortenedUrls.objects.all().order_by("-created_at")
+    queryset = ShortenedUrls.objects.order_by("-created_at")
     serializer_class = UrlListSerializer
     permission_classes = [permissions.IsAuthenticated] #로그인 된 user만 이 api를 이용할 수 있음
+
+    def create(self, request):
+        #POST METHOD
+        serializer = UrlCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            rtn = serializer.create(request, serializer.data)
+            return Response(UrlListSerializer(rtn).data, status=status.HTTP_201_CREATED)
+        pass
+
+    def retrieve(self, request, pk=None):
+        #Detail GET
+        queryset = self.get_queryset().filter(pk=pk).first()
+        serializer = UrlListSerializer(queryset)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        #PUT METHOD
+        pass
+
+    def partial_update(self, request, pk=None):
+        #PATCH METHOD
+        pass
+
+    def destroy(self, request, pk=None):
+        #DELETE METHOD
+        queryset = self.get_queryset().filter(pk=pk, creator_id=request.user.id)
+        if not queryset.exists():
+            raise Http404
+        queryset.delete()
+        url_count_changer(request, False)
+        return MsgOk()
+
+    def list(self, request):
+        # GET ALL
+        queryset = self.get_queryset().all()
+        serializer = UrlListSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
--- shortener/utils.py
+++ shortener/utils.py
@@ -1,3 +1,5 @@
+from rest_framework.response import Response
+
 from shortener.models import ShortenedUrls, Users
 from django.db.models import F
 
@@ -5,3 +7,7 @@ from django.db.models import F
 def url_count_changer(request, is_increase: bool):
     count_number = 1 if is_increase else -1
     Users.objects.filter(id=request.user.id).update(url_count=F('url_count') + count_number)
+
+
+def MsgOk(status: int= 200):
+    return Response({"msg":"ok"}, status=status)
